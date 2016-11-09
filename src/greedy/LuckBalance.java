f396dc9d24509d12cb8f2355614f84841159a706
jgvdangelo <jgvdangelo@gmail.com>
2016-11-09 18:10:28 -0500

    Adding working code for LuckBalance challenge

--- src/greedy/LuckBalance.java
+++ src/greedy/LuckBalance.java
@@ -10,6 +10,34 @@ public class LuckBalance {
 		int n = scan.nextInt();
 		int k = scan.nextInt();
 		
+		int balance = 0;
+		
+		int luck = 0;
+		int imp = 0;
+		
+		List<Integer> importantScores = new LinkedList<Integer>();
+		
+		for (int i = 0; i < n; i++) {
+			luck = scan.nextInt(); 
+			imp = scan.nextInt();
+			
+			if (imp == 0) {
+				balance += luck;
+			} else { // if imp == 1
+				importantScores.add(luck);
+			}
+		}
+		
+		Collections.sort(importantScores, Collections.reverseOrder());
+		
+		while (!importantScores.isEmpty() && k > 0) {
+			balance += importantScores.remove(0);
+		}
+		
+		while (!importantScores.isEmpty()) {
+			balance -= importantScores.remove(0);
+		}
+		
 		scan.close();
 	}
 }
